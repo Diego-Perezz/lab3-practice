544bbe5d7b56d388cb653c8aadf473408bc97ae9
Diego-Perezz <diego.perez19@upr.edu>
2023-02-10 12:30:16 -0400

    switch practice

---
+++ lab3_pract.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int main(){
+    int x = 1;
+    switch (x)
+    {
+    case 1:
+        cout << "is 1" << endl;
+        break;
+    case 2:
+        cout << "is 1" << endl;
+        break;
+
+    default:
+        cout << "other number" << endl;
+        break;
+    }
+    return 0;
+}
